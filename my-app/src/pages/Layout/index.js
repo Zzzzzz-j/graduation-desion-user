4d2b5b6379eb1f920ef511a191a79b07461feedf
zhujie <1421634618@qq.com>
2022-05-16 18:36:22 +0800

    bug: :bug:

--- my-app/src/pages/Layout/index.js
+++ my-app/src/pages/Layout/index.js
@@ -43,7 +43,6 @@ export default function AccountManage(props) {
     }, [])
 
     function logOut() {
-        console.log('logout');
         sessionStorage.removeItem('token');
         history('/login');
     }
@@ -64,8 +63,7 @@ export default function AccountManage(props) {
             console.log(res);
             if (res.status === 200) {
                 message.success('修改成功!');
-                userInfo.password = newPassword;
-                dispatch({ type: USERINFO, data: userInfo });
+                logOut();
             } else {
                 message.error(res.message);
             }
